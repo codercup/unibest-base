3600df13b14995ff4cfb521b7e382c866d427a79
菲鸽 <1020103647@qq.com>
2024-04-15 14:34:24 +0800

    feat: 增加 VITE_ENV_FILE 环境变量

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -27,6 +27,10 @@
       模板分支是：
       <text class="text-green-500">base</text>
     </view>
+    <view class="text-center mt-4">
+      应用的.env文件是：
+      <text class="text-green-500">{{ VITE_ENV_FILE }}</text>
+    </view>
   </view>
 </template>
 
@@ -34,7 +38,7 @@
 import PLATFORM from '@/utils/platform'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
-
+const { VITE_ENV_FILE } = import.meta.env
 const author = ref('菲鸽')
 const description = ref(
   'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
